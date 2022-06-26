9e81b6ebc2687c780501920512c1856b56eaec7d
Lucas Ribeiro <lucas.rdiniz01@gmail.com>
2022-06-26 16:37:13 -0300

    Testes de JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,22 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
+
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber >= 0){
+        currentNumberWrapper.style.color = 'black';
+    }
 }
 
 function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red';
+    }
 }
+
 function zerar(){
     currentNumber = 0;
     currentNumberWrapper.innerHTML = currentNumber;
